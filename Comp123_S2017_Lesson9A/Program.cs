ecdf2b2d4a6a5f20cd0661f9e5e251cd1efac50f
Khim Tang <ktang33@my.centennialcollege.ca>
2017-07-12 00:38:04 -0400

    Displayed all powers to the Powers List - in the Driver/Program class

--- Comp123_S2017_Lesson9A/Program.cs
+++ Comp123_S2017_Lesson9A/Program.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
  * Name: Khim Tang
  * Date: July 11, 2017
  * Description: Demo for Lesson 9
- * Version: 0.3 - Added a Power to the SuperHuman object
+ * Version: 0.4 - Added another Power to the SuperHuman object
+ *              - Also displayed all the powers in the Powers List
  */
 
 
@@ -20,6 +21,8 @@ namespace Comp123_S2017_Lesson9A
         {
             SuperHuman superHuman = new SuperHuman("Super Dude");
             superHuman.AddPower("Spider Climing", 50);
+            superHuman.AddPower("Webcasting", 40);
+            superHuman.DisplayPowers();
         }
     }
 }
