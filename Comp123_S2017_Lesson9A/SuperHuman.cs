a853cff05aa639c6ae53f7b867fa15b000e7e2de
Khim Tang <ktang33@my.centennialcollege.ca>
2017-07-11 22:49:53 -0400

    Added SuperHuman class

---
+++ Comp123_S2017_Lesson9A/SuperHuman.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Khim Tang
+ * Date: July 11, 2017
+ * Description: This is the SuperHuman class that extends the Human abstract class
+ * Version: 0.1 - Created the SuperHuman class
+ */
+
+namespace Comp123_S2017_Lesson9A
+{
+    /// <summary>
+    /// This is the SuperHuman class.
+    /// </summary>
+    public class SuperHuman : Human
+    {
+        // PRIVATE INSTANCE VARIABLES (FIELDS)
+
+        // PUBLIC PROPERTIES
+
+        // CONSTRUCTORS ---------------------------
+        public SuperHuman(string name) : base(name)
+        {
+                
+        }
+
+
+        // PRIVATE METHODS
+
+        // PUBLIC METHODS
+    }
+}
