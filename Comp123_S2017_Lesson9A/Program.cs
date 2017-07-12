4e6b6183539808b7d891d6f171707bd19a83650a
Khim Tang <ktang33@my.centennialcollege.ca>
2017-07-11 21:36:58 -0400

    Modified Program.cs

--- Comp123_S2017_Lesson9A/Program.cs
+++ Comp123_S2017_Lesson9A/Program.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*
+ * Name: Khim Tang
+ * Date: July 11, 2017
+ * Description: Demo for Lesson 9
+ * Version: 0.1 - Created the Program class
+ */
+
+
 
 namespace Comp123_S2017_Lesson9A
 {
