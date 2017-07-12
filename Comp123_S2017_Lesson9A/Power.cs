3eb5c2280a1cb32dfdecff54da0271d9d15daf35
Khim Tang <ktang33@my.centennialcollege.ca>
2017-07-11 23:00:13 -0400

    Added Power class

---
+++ Comp123_S2017_Lesson9A/Power.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Khim Tang
+ * Date: July 11, 2017
+ * Description: Demo for Lesson 9
+ * Version: 0.1 - Created the Power class
+ */
+
+namespace Comp123_S2017_Lesson9A
+{
+    /// <summary>
+    /// This is the Power class which will be used as Data type
+    /// </summary>
+    public class Power
+    {
+        // PUBLIC PROPERTIES
+        public string Name { get; set; }
+        public int Rank { get; set; }
+
+        // CONSTRUCTORS ---------------------------
+
+    }
+}
