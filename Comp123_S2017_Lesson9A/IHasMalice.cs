1d5a126dfbb7f1e0cb5012e30f23ce9dc8211caf
Khim Tang <ktang33@my.centennialcollege.ca>
2017-07-13 15:21:33 -0400

    Added IHasMalice Interface

---
+++ Comp123_S2017_Lesson9A/IHasMalice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Khim Tang
+ * Date: July 13, 2017
+ * Description: This Interface lists the signature for any Super Villain Subclass
+ * Version: 0.1 - declared the Malice properties
+ */
+
+namespace Comp123_S2017_Lesson9A
+{
+    public interface IHasMalice
+    {
+        // Public properties
+        int Malice { get; set; }
+    }
+}
